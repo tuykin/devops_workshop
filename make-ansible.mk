f27b39f6755837ecde53e79da3cd581a8da1a5bc
Anvar Tuykin <tuykin@gmail.com>
2018-09-02 16:58:29 +0300

    Add ansible deploy

--- make-ansible.mk
+++ make-ansible.mk
@@ -1,13 +1,13 @@
-FILE = "ansible/production/group_vars/all/vault.yml"
+VPF = "ansible/production/group_vars/all/vault.yml"
 
 ansible-vaults-encrypt:
-	ansible-vault encrypt $(FILE)
+	ansible-vault encrypt $(VPF)
 
 ansible-vaults-decrypt:
-	ansible-vault decrypt $(FILE)
+	ansible-vault decrypt $(VPF)
 
 ansible-vaults-edit:
-	ansible-vault edit $(FILE)
+	ansible-vault edit $(VPF)
 
 ansible-deps-install:
 	ansible-galaxy install -r requirements.yml
--- make-production.mk
+++ make-production.mk
@@ -1,2 +1,8 @@
+U := root
+VPF := tmp/ansible-vault-password
+
 production-setup:
-	ansible-playbook ansible/site.yml -i ansible/production -u root -vv # --ask-vault-pass
+	ansible-playbook ansible/site.yml -i ansible/production -u $U -vv --vault-password-file=$(VPF)
+
+production-deploy:
+	ansible-playbook ansible/deploy.yml -i ansible/production -u $U -vv --ask-vault-pass
